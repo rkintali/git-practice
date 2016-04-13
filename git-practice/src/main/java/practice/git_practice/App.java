2803613e83952c8ab3d04356e98718b27ba730b8
RK <rajsekhar.kintali@gmail.com>
2016-04-14 06:29:05 +1000

    added feature1 documentation

--- git-practice/src/main/java/practice/git_practice/App.java
+++ git-practice/src/main/java/practice/git_practice/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!123" );
+        // feature one's focus is more on elaborate documentation.
+        System.out.println( "This line is introduced in feature 1 branch" );
     }
 }
